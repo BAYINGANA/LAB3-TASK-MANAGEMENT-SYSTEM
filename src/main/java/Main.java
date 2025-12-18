e46e22b8a5fd69982fa0eb264f0c1d4adab7622b
BAYINGANA <bayinganadivine@gmail.com>
2025-12-18 10:12:44 +0200

    feat: added file based persistence for users, projects and tasks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,25 @@
+
+
 import utils.ConsoleMenu;
+import utils.FileUtils;
+import services.ProjectService;
+import services.UserService;
+import services.TaskService;
+import models.UserCatalog;
+import models.ProjectCatalog;
+import models.TaskCatalog;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleMenu menu = new ConsoleMenu();
+        Map<String, List<?>> loaded = FileUtils.loadAll();
+        UserService userService = new UserService((List<UserCatalog>) loaded.get("users"));
+        ProjectService projectService = new ProjectService((List<ProjectCatalog>) loaded.get("projects"));
+        TaskService taskService = new TaskService((List<TaskCatalog>) loaded.get("tasks"));
+        ConsoleMenu menu = new ConsoleMenu(projectService, userService, taskService);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            FileUtils.saveAll(userService.getAllUsers(), projectService.getAllProjects(), taskService.getAllTasks());
+        }));
         menu.MainMenu();
     }
 }
